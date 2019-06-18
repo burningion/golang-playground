527a979d40c56d2dbd7a3b426cd3f3dc9a0c349d
Kirk Kaiser <kirk@zothcorp.com>
2019-06-18 15:02:59 -0400

    Traces and Logs in both places

--- main.go
+++ main.go
@@ -1,26 +1,50 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"strings"
+
+	log "github.com/Sirupsen/logrus"
 
 	httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
+	span, _ := tracer.SpanFromContext(r.Context())
+	traceID := span.Context().TraceID()
+	spanID := span.Context().SpanID()
+
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
+
+	log.WithFields(log.Fields{
+		"dd.trace_id": traceID,
+		"dd.span_id":  spanID,
+		"message":     message,
+	}).Info("root url called with " + message)
+
 	message = "Hello " + message
+
 	w.Write([]byte(message))
 }
 
 func sayPong(w http.ResponseWriter, r *http.Request) {
+	span, _ := tracer.SpanFromContext(r.Context())
+	traceID := span.Context().TraceID()
+	spanID := span.Context().SpanID()
+
+	log.WithFields(log.Fields{
+		"dd.trace_id": traceID,
+		"dd.span_id":  spanID,
+	}).Info("Ping / Pong request called")
 	w.Write([]byte("pong"))
 }
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.Info("starting up")
+
 	// start the tracer with zero or more options
 	tracer.Start(tracer.WithServiceName("test-go"))
 	defer tracer.Stop()
